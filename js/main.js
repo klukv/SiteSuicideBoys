ab0119448252ed75e30549aea05d654b442e4058
klukv <tema200260@gmail.com>
2022-12-08 22:48:41 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -452,3 +452,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		slider.next();
 	}
 });
+var a = document.getElementsByTagName("audio");
+function other_pause(b) {
+	for (i = 0; i < a.length; i++) {
+		if (!(i == b)) { a[i].pause() };
+	}
+}
